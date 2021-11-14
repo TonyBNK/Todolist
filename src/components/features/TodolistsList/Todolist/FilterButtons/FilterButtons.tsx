3cf4a3eed2725e35c0b624704fbb50f2aefbf6e8
TonyBNK <borisenk-anton@yandex.ru>
2021-11-14 15:26:45 +0300

    20. Refactoring of FilterButtons component.

--- src/components/features/TodolistsList/Todolist/FilterButtons/FilterButtons.tsx
+++ src/components/features/TodolistsList/Todolist/FilterButtons/FilterButtons.tsx
@@ -4,32 +4,25 @@ import {Button} from "@material-ui/core";
 import {FilterType, TodolistType} from "../../../../../types/types";
 
 
-export type FilterButtonsType = {
+export type FilterButtonsPropsType = {
     item: TodolistType
     changeItem: (model: TodolistType) => void
 }
-export const FilterButtons: React.FC<FilterButtonsType> = React.memo((
+export const FilterButtons: React.FC<FilterButtonsPropsType> = React.memo((
     {
         item,
         changeItem
     }) => {
     const {entityStatus} = item;
 
-    const onAllFilterHandler = useCallback(
-        () => changeItem({...item, filter: 'All'}), [changeItem, item]
-    );
-    const onActiveFilterHandler = useCallback(
-        () => changeItem({...item, filter: 'Active'}), [changeItem, item]
-    );
-    const onCompletedFilterHandler = useCallback(
-        () => changeItem({...item, filter: 'Completed'}), [changeItem, item]
+    const onFilterButtonClickHandler = useCallback(
+        (filter: FilterType) => changeItem({...item, filter}), [changeItem, item]
     );
 
-    const renderFilterButton = (onClick: () => void, filter: FilterType) => {
-
+    const renderFilterButton = (onClick: (filter: FilterType) => void, filter: FilterType) => {
         return <Button
             style={{margin: '3px'}}
-            onClick={onClick}
+            onClick={() => onFilterButtonClickHandler(filter)}
             color={'primary'}
             variant={item.filter === filter ? 'contained' : 'outlined'}
             size={'small'}
@@ -41,9 +34,9 @@ export const FilterButtons: React.FC<FilterButtonsType> = React.memo((
 
     return (
         <div className={c.filterButtons}>
-            {renderFilterButton(onAllFilterHandler, 'All')}
-            {renderFilterButton(onActiveFilterHandler, 'Active')}
-            {renderFilterButton(onCompletedFilterHandler, 'Completed')}
+            {renderFilterButton(onFilterButtonClickHandler, 'All')}
+            {renderFilterButton(onFilterButtonClickHandler, 'Active')}
+            {renderFilterButton(onFilterButtonClickHandler, 'Completed')}
         </div>
     );
 });
